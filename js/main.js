000f2ec36702d6f8587de3dd70a54018c1abe584
Anderson <anderson.ss.dev@gmail.com>
2025-08-04 23:23:17 -0300

    :sparkles: feat(main): Adicionando manipulação do form

--- js/main.js
+++ js/main.js
@@ -1,6 +1,24 @@
+import api from "./api.js";
 import ui from "./ui.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   ui.renderizarPensamentos();
   ui.limparCampos();
+
+  const formularioPensamento = document.getElementById("pensamento-form");
+  formularioPensamento.addEventListener('submit', manipularSubmissaoFormulario);
 });
+
+async function manipularSubmissaoFormulario(event){
+  event.preventDefault();
+  const id = document.getElementById("pensamento-id").value
+  const conteudo = document.getElementById("pensamento-conteudo").value
+  const autoria = document.getElementById("pensamento-autoria").value
+
+  try {
+    await api.salvarPensamento({conteudo, autoria});
+    ui.renderizarPensamentos();
+  } catch (error) {
+    alert(error)
+  }
+}
